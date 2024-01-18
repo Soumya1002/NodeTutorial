a49459e974dcae2d245648f7b9720a969086324f
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-18 14:15:55 +0530

    createserver and sending request and response

---
+++ app.js
@@ -0,0 +1,26 @@
+const http=require('http');
+
+
+const server=http.createServer((req,res)=>{
+    const url=req.url;
+  
+
+    if(url==='/'){
+       // res.setHeader('Content-type','text/html');
+        res.write('<html>'); 
+        res.write('<head><title>Enter Details</title></head>');
+        res.write('<body><h1>Hello from LOGIN PAGE</h1>');  
+        res.write('<form action="/message" method="POST">Message:<input type="text" id="message"><button type="submit" value="send">Send</button></form></body>');  
+        res.write('</html>');
+        return res.end();
+    }
+    //res.setHeader('Content-type','text/html');
+   
+    res.write('<html>'); 
+    res.write('<head><title>Nodejs tutorial</title></head>');
+    res.write('<body><h1>Hello from App.js</h1></body>');  
+    res.write('</html>');
+    res.end();
+   
+});
+server.listen(3000);

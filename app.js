f9ef9bf0afc1179e0a362f33f9ed4369cfc7baee
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-18 16:10:39 +0530

    getting form input and adding to hello.txt file

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-const http=require('http');
-const fs=require('fs');
+const http = require('http');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-  
-
-    if(url==='/'){
-       res.setHeader('Content-type','text/html');
-        res.write('<html>'); 
+    if (url === '/') {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
         res.write('<head><title>Enter Details</title></head>');
-        res.write('<body><h1>Hello from LOGIN PAGE</h1>');  
-        res.write('<form action="/message" method="POST">Message:<input type="text" id="message"><button type="submit" value="send">Send</button></form></body>');  
+        res.write('<body><form action="/message" method="POST" enctype="application/x-www-form-urlencoded">Message:<input type="text" name="message"><button type="submit" value="send">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
 
-    if(url==='/message' && method=='POST'){
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.setHeader('Location','/');
-        res.statusCode=302;
-        return res.end();
-    }
+    if (url === '/message' && method === 'POST') {
+        const body=[];
 
-    res.setHeader('Content-type','text/html');   
-    res.write('<html>'); 
+        req.on('data', (chunk) => {
+           // console.log(chunk);
+            body.push(chunk);
+        });
+
+
+
+        req.on('end', () => {
+            const parsedBody=Buffer.concat(body).toString();
+            console.log(parsedBody);
+        })
+           
+            fs.writeFileSync('hello.txt', "DUMMY");
+
+                res.setHeader('Location', '/');
+                res.statusCode = 302;
+                return res.end();
+            
+        }
+
+     
+    
+
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
     res.write('<head><title>Nodejs tutorial</title></head>');
-    res.write('<body><h1>Hello from App.js</h1></body>');  
+    res.write('<body><h1>Hello from App.js</h1></body>');
     res.write('</html>');
-    res.end();
-   
+    return res.end();
 });
-server.listen(5000);
+
+server.listen(8000);
